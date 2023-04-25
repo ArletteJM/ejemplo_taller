59b0b28e3fc55a56d05a19c6058a8b54c4a7c8d0
ArletteJM <107292396+ArletteJM@users.noreply.github.com>
2023-04-25 10:08:09 -0600

    Template adminLTE agregado

--- application/controllers/Taller.php
+++ application/controllers/Taller.php
@@ -2,7 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Taller extends CI_Controller {
-	
+
+	public function __construct(){
+		//El constructor lleva 2_ _
+		//hereda los metodos de la superclase CI_Controler 
+	parent::__construct();	
+	//Agregar helper y librerias adicionales al controlador
+	$this->load->helper('url'); //Para ocupar una funcion que se llama url base 
+	}
+
 	public function index()
 	{
 		$this->load->view('Taller/index_view');
@@ -14,5 +22,10 @@ class Taller extends CI_Controller {
 
     public function agregar_taller(){
 		$this->load->view('Taller/agregar_taller_view');
+	}
+	//vista blank page 
+	
+	public function black_view(){
+		$this->load->view('Taller/blank');
 	}
 }
